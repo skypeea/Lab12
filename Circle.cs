b231bf1661920c43eb988ec37ed011d1114c555b
Алексей <lebedevalexeyo@gmail.com>
2022-10-18 23:08:57 +1100

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    static class Circle
+    {
+
+
+        public static double GetCircle(double r)//определение длины окружности по радиусу
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double GetArea(double r)//определение площади по радиусу
+        {
+            return r * r * Math.PI;
+        }
+        public static bool IsBelong(double x0, double y0, double r, double x, double y)//x0,y0 - координаты центра круга, x,y - коордиаты точки
+        {
+            double a = x - x0;
+            double b = y - y0;
+            return (Math.Sqrt((a * a) + (b * b)) <= r);
+           
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты центра окружности");
+            Console.WriteLine("x0:");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y0:");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты точки");
+            Console.WriteLine("x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Длина окружности - " +Circle.GetCircle(r));
+            Console.WriteLine("Площадь круга - " +Circle.GetArea(r));
+           if (Circle.IsBelong(x0,y0,r,x,y))
+                {
+                Console.WriteLine("Точка принадлежит окружности");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит окружности");
+            }
+            Console.ReadKey();
+        }
+    }
+}
